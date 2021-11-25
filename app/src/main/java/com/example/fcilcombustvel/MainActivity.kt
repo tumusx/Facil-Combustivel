826dade53ae68ff6b9f16a984cde8287e1dad1f6
Murillo <tumusx1@gmail.com>
2021-11-25 01:15:26 -0300

    First commit and create project / design inicial

---
+++ app/src/main/java/com/example/fcilcombustvel/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.fcilcombustvel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewDebug
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+            setContentView(R.layout.activity_main)
+        val precoGasolina = findViewById(R.id.gasolinaID) as EditText
+        val precoAlcool  = findViewById(R.id.alcoolID) as EditText
+        val modeloCarro = findViewById(R.id.modeloCarroID) as EditText
+        val botãoNexT = findViewById(R.id.button) as Button
+
+        botãoNexT.setOnClickListener {
+        }
+
+    }
+}
