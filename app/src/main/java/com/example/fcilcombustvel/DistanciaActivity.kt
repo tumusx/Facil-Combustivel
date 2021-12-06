b5eb435efbfc35778fd2686e215ff587f8efdbb1
Murillo Alves <tumusx1@gmail.com>
2021-12-05 21:39:02 -0300

    teste

--- app/src/main/java/com/example/fcilcombustvel/DistanciaActivity.kt
+++ app/src/main/java/com/example/fcilcombustvel/DistanciaActivity.kt
@@ -1,21 +1,26 @@
 package com.example.fcilcombustvel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class DistanciaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_distancia)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_distancia)
+        val nextBootom = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.textView2).apply {
+            text = nextBootom.toString()
+        }}}
 
-            val calcularPreco = findViewById<Button>(R.id.btCalcular)
-            val distanciaKM = findViewById<EditText>(R.id.rvDistanciaIda)
-
-
-
+       /* val calcularPreco = findViewById<Button>(R.id.btCalcular)
+        val distanciaKM = findViewById<EditText>(R.id.rvDistanciaIda)*/
+       private fun Intent.getIntExtra(extraMessage: String) {
 
 
     }
-}
--- app/src/main/java/com/example/fcilcombustvel/MainActivity.kt
+++ app/src/main/java/com/example/fcilcombustvel/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.fcilcombustvel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
 import android.view.ViewDebug
 import android.widget.Button
 import android.widget.EditText
@@ -12,6 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContentView(R.layout.activity_main)
+        fun sendMessage (view: View){
+            val editText = findViewById<EditText>(R.id.alcoolID)
+            val message = editText.text.toString()
+            val intent = Intent (this, DistanciaActivity::class.java).apply{
+                putExtra(EXTRA_MESSAGE, message)
+
+            }
+
         val precoGasolina = findViewById<EditText>(R.id.gasolinaID)
         val precoAlcool = findViewById<EditText>(R.id.alcoolID)
         val nextBootom = findViewById<Button>(R.id.nextBootom)
@@ -22,6 +33,16 @@ class MainActivity : AppCompatActivity() {
             val gasolina1 = Integer.parseInt(precoGasolina.text.toString())
         }
 
+    }
+
+
+        startActivity(intent)
+
+
 
     }
+    private fun putExtra(extraMessage: String, callButton: Button?) {
+
+    }
+
 }
